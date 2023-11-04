c4dcb3817ab55ae8991f5076fcfc4c58226549b5
AdityaSuple <supleaditya@gmail.com>
2023-11-04 13:30:23 +0530

    files added

---
+++ src/main/java/com/aditya/springboot/repository/EmpRepo.java
@@ -0,0 +1,11 @@
+package com.aditya.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aditya.springboot.entity.Employee;
+
+@Repository
+public interface EmpRepo extends JpaRepository<Employee, Integer>{
+	
+}
